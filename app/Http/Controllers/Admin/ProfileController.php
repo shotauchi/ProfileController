6fab0da59002a2c638e0172b6737d6b8468cc256
[shotauchi] <[tianneixiang84@gmail.com]>
2024-11-21 16:22:15 +0000

    コミット01

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -6,24 +6,26 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
-
 {
+
     public function add()
-{
-return view('admin.profile.create');
-}
-public function create()
-{
-    return redirect('admin/profile/create');
-}
-public function edit()
-{
-    return view('admin.profile.edit');
-}
+    {
+        return view('admin.profile.create');
+    }
 
-public function update()
-{
-    return redirect('admin/profile/edit');
-}
+    public function create()
+    {
+        return redirect('admin/profile/create');
+    }
 
+    public function edit()
+    {
+        return view('admin.profile.edit');
+    }
+    
+    public function update()
+    {
+        return redirect('admin/profile/edit');
+    }
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,47 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Laravel04-practice03
+// 「http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください
+// /XXX というアクセスしたら AAAController の bbb Action
+// 
+// use App\Http\Controllers\Admin\NewsController;// use 宣言で使うコントローラを指定する（重複しないように）
+// Route::controller(NewsController::class)->prefix('admin')->group(function() {
+//     Route::get('profile/create', 'add');// /admin/profile/create
+// });
+
+// Laravel04-practice04
+// 【応用】 前章でAdmin/ProfileControllerを作成し、add Action, edit Actionを追加しました。
+// web.phpを編集して、admin/profile/create にアクセスしたら ProfileController の add Action に、
+// admin/profile/edit にアクセスしたら ProfileController の edit Action に割り当てるように設定してください
+use App\Http\Controllers\Admin\NewsController;// use 宣言で使うコントローラを指定する（重複しないように）
+Route::controller(NewsController::class)->prefix('admin')->group(function() {
+//                                        ^^^^^^^^^^^^^^^ prefix() メソッドで、アクセス URL の接頭辞を指定する
+//                ^^^^^^^^^^^^^^^^^^^^^ 使用するコントローラクラスを指定する
+    // admin/profile/create にアクセスしたら ProfileController の add Action
+    Route::get('profile/create', 'add');// /admin/profile/create
+    //                           ^^^^^ コントローラアクション名を指定する
+    //         ^^^^^^^^^^^^^^^^ アクセス URL を指定する
+    
+    // admin/profile/edit にアクセスしたら ProfileController の edit Action
+});
+
+// シンプルなルーティング設定
+use App\Http\Controllers\UserController;// use 宣言で使うコントローラを指定する（重複しないように）
+Route::get('/user', [UserController::class, 'index']);
+//                                          ^^^^^^^ アクション名の指定
+//                   ^^^^^^^^^^^^^^^^^^^^^ コントローラクラスの指定
+//                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ get() メソッドの第２引数で配列を使って
+//                                                   コントローラクラスとアクション名を指定する
+//         ^^^^^^^ アクセス URL を指定する
+
+// カリキュラムの少し複雑なルーティング設定
+// use App\Http\Controllers\Admin\NewsController;// use 宣言で使うコントローラを指定する（重複しないように）
+// Route::controller(NewsController::class)->prefix('admin')->group(function() {
+//     Route::get('news/create', 'add');
+// });
+// ↑↑↑書き換えてみる↓↓↓
+// シンプルなルーティング設定
+use App\Http\Controllers\Admin\NewsController;// use 宣言で使うコントローラを指定する（重複しないように）
+Route::get('/admin/news/create', [NewsController::class, 'add']);
